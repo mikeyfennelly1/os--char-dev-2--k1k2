ac8685e10e532c3d514166b5db7e6fc04984a584
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-12 14:28:03 +0000

    test_job updated

--- test/job/test_job.c
+++ test/job/test_job.c
@@ -2,6 +2,9 @@
 #include <CUnit/Basic.h>
 #include "../../src/device/job/job.h"
 
+#define TEST_KEY "cpu_speed_hz"
+#define TEST_VALUE "3000Hz"
+
 // job data structure to get CPU 
 // information from kernel space
 static struct Job cpu = {
@@ -16,16 +19,34 @@ static struct Job cpu = {
 
 static key_value_pair* get_cpu_speed_hz(void)
 {
-    add_step_to_job(&cpu, &get_cpu_speed_hz);
-    
-    char** job_data = run_job(&cpu);
+    // malloc the key_value_pair
+    key_value_pair* cpu_speed_step_kvp = (key_value_pair *) malloc(sizeof(key_value_pair));
 
-    free(data_buffer);
+    // set the fields of the key_value_pair you just malloc'd
+    //
+    // NOTE: Yours will be retrieving data from kernel space,
+    //       I just put in some dummy data for this example.
+    cpu_speed_step_kvp->key = TEST_KEY;
+    cpu_speed_step_kvp->value = TEST_VALUE;
+
+    // return pointer to key_value_pair on heap
+    return cpu_speed_step_kvp;
 };
 
+// assert that the JSON string returned from 
+// the above CPU job is as expected 
 void test_cpu_job_output(void)
 {
-    
+    char* cpu_json = run_job(&cpu);
+    printf("%d\n", cpu_json);
+
+    char* actual = run_job(&cpu);
+    char* expected = "{\n\tcpu_speed_hz:3000Hz\n}";
+
+    CU_ASSERT_STRING_EQUAL(
+        actual,
+        expected
+    );
 }
 
 int main(void)
@@ -44,7 +65,7 @@ int main(void)
         return CU_get_error();
     }
 
-    if (!CU_add_test(suite, "test_job", test_job))
+    if (!CU_add_test(suite, "test_job", test_cpu_job_output))
     {
         CU_cleanup_registry();
         return CU_get_error();
