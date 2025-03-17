5fac440a565194e7dd65e5e8c7c4776e7a4f01c7
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-17 13:46:29 +0000

    dummy disk info added, script updated to change device perms after init

--- src/disk.c
+++ src/disk.c
@@ -1,7 +1,59 @@
+#include <asm/cpu.h>
+#include <linux/kernel.h>
 #include <linux/slab.h>
+#include <asm/msr.h>
 #include "job.h"
+
+key_value_pair disk_model(void) { 
+    key_value_pair keyVal;
+    keyVal.key = "disk_model";
+    keyVal.value = "dummy_value";
+    return keyVal;
+}
+
+key_value_pair disk_vendor(void) {
+    key_value_pair keyVal;
+    keyVal.key = "disk_vendor";
+    keyVal.value = "dummy_value";
+    return keyVal;
+}
+
+key_value_pair disk_frequency(void) {
+    key_value_pair keyVal;
+    keyVal.key = "disk_frequency";
+    keyVal.value = "dummy_value";
+    return keyVal;
+}
+
+key_value_pair disk_cores(void) {
+    key_value_pair keyVal;
+    keyVal.key = "disk_cores";
+    keyVal.value = "dummy_value";
+    return keyVal;
+}
+
+key_value_pair disk_load(void) {
+    key_value_pair keyVal;
+    keyVal.key = "disk_load";
+    keyVal.value = "dummy_value";
+    return keyVal;
+}
+ 
+key_value_pair disk_idle_time(void) {
+    key_value_pair keyVal;
+    keyVal.key = "disk_idle_time";
+    keyVal.value = "dummy_value";
+    return keyVal;
+}
 
 Job* get_disk_job(void)
 {
-    return NULL;
+    Job* disk_info = job_init("disk", &disk_model);
+    append_step_to_job(disk_info, &disk_vendor);
+    append_step_to_job(disk_info, &disk_frequency);
+    append_step_to_job(disk_info, &disk_cores);
+    append_step_to_job(disk_info, &disk_load);
+    append_step_to_job(disk_info, &disk_idle_time);
+
+    return disk_info;
 }
