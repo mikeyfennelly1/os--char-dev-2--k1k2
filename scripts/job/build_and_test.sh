0b0f8425637e9420336a73d7648c69f9cf412a8a
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-22 00:33:33 +0000

    remove scripts dir

--- scripts/job/build_and_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# compile {BUILD_DIR}/job.o
-./scripts/job/compile_job.sh
-# compile {BUILD_DIR}/tests/test_job
-./scripts/job/compile_job_tests.sh
-./build/test/test_job
--- scripts/job/compile_job_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-test_build_dir="./build/test"
-test_src="./test/job/test_job.c"
-mkdir -p ${test_dir}
-
-gcc -Wall -Wextra ${test_src} -o ${test_build_dir}/test_job -lcunit -ljson-c -I./${test_build_dir}
